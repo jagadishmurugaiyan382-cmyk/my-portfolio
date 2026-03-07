5244012ff1a2d7980d33339dfbb0f73a6f43c651
jagadishmurugaiyan382-cmyk <jagadishmurugaiyan382@gmail.com>
2026-03-07 19:18:25 +0530

    fix home rote

--- app1.py
+++ app1.py
@@ -1,26 +1,29 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/index1')
+@app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 @app.route('/login', methods=['GET','POST'])
 def login():
 
     if request.method == 'POST':
-
         username = request.form['username']
         password = request.form['password']
 
-        if username == "admin" and password == "1234":
-            return "Login Successful 🔐"
+        if username == "jagadeesh" and password == "1234":
+            return "Login Successful"
 
         else:
             return "Invalid Login"
 
     return render_template("login.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "_main_":
+    app.run(debug=True)
